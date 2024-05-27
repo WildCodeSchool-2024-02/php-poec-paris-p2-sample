514edb43228d0a8a07b2a330849a9c64013c6ba7
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-05-27 17:55:37 +0200

    Insert new task into db

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -19,6 +19,25 @@ class TaskController extends AbstractController
      */
     public function add(): string
     {
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $task = array_map('htmlentities', array_map('trim', $_POST));
+
+            if (empty($task['label'])) {
+                $errors['label'] = 'Enter a label';
+            }
+
+            if (empty($task['priority'])) {
+                $errors['priority'] = 'Enter a priority';
+            }
+
+            if (empty($errors)) {
+                $task['status'] = TaskManager::STATUS_PENDING;
+
+                $this->taskManager->insert($task);
+            }
+        }
 
         return $this->twig->render('Task/add.html.twig', [
             'errors' => $errors,
