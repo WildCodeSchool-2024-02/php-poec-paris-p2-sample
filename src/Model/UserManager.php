94b16387bb62700ab6e863b559ccd3faad52ae0f
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-06-05 17:29:10 +0200

    Finish register

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -8,6 +8,24 @@ class UserManager extends AbstractManager
 {
     public const TABLE = 'user';
 
+    public function insert(array $user): int|false
+    {
+        $query = 'INSERT INTO user (firstname, lastname, pseudo, password, avatar) ';
+        $query .= 'VALUES (:firstname, :lastname, :pseudo, :password, :avatar);';
+
+        $statement = $this->pdo->prepare($query);
+
+        $statement->bindValue(':firstname', $user['firstname'], PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $user['lastname'], PDO::PARAM_STR);
+        $statement->bindValue(':pseudo', $user['pseudo'], PDO::PARAM_STR);
+        $statement->bindValue(':password', $user['password'], PDO::PARAM_STR);
+        $statement->bindValue(':avatar', $user['avatar'], PDO::PARAM_STR);
+
+        $statement->execute();
+
+        return $this->pdo->lastInsertId();
+    }
+
     public function selectOneByPseudo(string $pseudo): array|false
     {
         $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE pseudo=:pseudo");
